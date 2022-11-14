6fedf8fa3cfef7c3b4259dc9a05bd5d69c767777
Clément Daguenet <99010993+ClementDaguenet@users.noreply.github.com>
2022-11-14 23:29:01 +0100

    Delete

--- TD2_JS_JQuery/td2Ex2/public_html/calculatrice.js
+++
@@ -1,9 +0,0 @@
-function calcul()
-{
-  let val1;
-  let val2;
-
-  val1 = window.document.forms["calculatrice"].val1.value;
-  val2 = window.document.forms["calculatrice"].val2.value;
-  window.document.forms["calculatrice"].resu.value = val1 * val2;
-}
