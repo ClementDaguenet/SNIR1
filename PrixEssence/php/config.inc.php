65491c909f76095f966da41c4c78efebaf36c003
Clément Daguenet <99010993+ClementDaguenet@users.noreply.github.com>
2022-11-14 23:17:23 +0100

    Delete

--- PrixEssence/php/config.inc.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-define("SERVEURBD", "172.18.58.5");
-define("LOGIN", "snir");
-define("MOTDEPASSE", "snir");
-define("NOMDELABASE", "tarifEssence");
-?>
--- PrixEssence/php/traitementEssence.php
+++
@@ -1,6 +0,0 @@
-<?php
-
-require_once 'ModeleEssence.php';
-
-$prix = new ModeleEssence();
-echo $prix->ObtenirPrix();
